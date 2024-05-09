e4645c49b52ad766c3904248789b2afcd1fcb4fc
CorvoHyatt <corvohyatt@gmail.com>
2024-05-08 21:23:17 -0600

    combinatory generator created

--- genetic_algorithm/population_generators/permutation_generator.py
+++ genetic_algorithm/population_generators/permutation_generator.py
@@ -1,15 +1,18 @@
+from typing import List
+
 import numpy as np
 
 from genetic_algorithm.Individuals.individual import Individual
 
 
-class CombinatoryPopulationGenerator:
-    
+class PermutationPopulationGenerator:
     @staticmethod
-    def generate_population(population_size: int, limits: int):
+    def generate_population(population_size: int, limits: int) -> List[Individual]:
+
         population = set()
         while len(population) < population_size:
-            genotype = np.random.randint(1, limits + 1, size=limits)
+            genotype = np.arange(1, limits + 1)
+            np.random.shuffle(genotype)
             individual = Individual(genotype)
             population.add(individual)
 
