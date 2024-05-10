520ef94e3cf3084abd021f06cb648fc3da46eb5c
CorvoHyatt <corvohyatt@gmail.com>
2024-05-10 09:04:54 -0600

    ordered Crosover created

--- genetic_algorithm/operators/Crossover/ordered_crossover.py
+++ genetic_algorithm/operators/Crossover/ordered_crossover.py
@@ -1,3 +1,4 @@
+import random
 from typing import List
 
 import numpy as np
@@ -5,45 +6,64 @@ from genetic_algorithm.Individuals.individual import Individual
 from genetic_algorithm.operators.Crossover.crossover_operators import CrossoverOperator
 
 
-class Ordered(CrossoverOperator):
+class OrderedOX1(CrossoverOperator):
+    "No apto para cardiacos ni para codificación binaria ni combinatoria"
+
     @staticmethod
     def crossover(population_selected: List[Individual]) -> List[Individual]:
         new_population = []
         for i in range(len(population_selected) - 1):
             parent1 = population_selected[i]
             parent2 = population_selected[i + 1]
+            point1 = random.randint(0, len(parent1.genotype) - 2)
 
-            # Realizamos la cruza ordenada OX1
-            start = np.random.randint(0, len(parent1.genotype))
-            end = np.random.randint(start + 1, len(parent1.genotype))
-            child_genotype = [-1] * len(parent1.genotype)
-            child_genotype[start:end] = parent1.genotype[start:end]
-            index = end
-            for g in parent2.genotype[end:] + parent2.genotype[:end]:
-                if g not in child_genotype:
-                    child_genotype[index % len(parent1.genotype)] = g
-                    index += 1
+            while True:
+                point2 = random.randint(0, len(parent1.genotype) - 2)
+                if point2 != point1:
+                    break
+
+            start_point = min(point1, point2) + 1
+            end_point = max(point1, point2) + 2
+            genotype = [-1] * len(parent1.genotype)
+            genotype[start_point:end_point] = parent2.genotype[start_point:end_point]
+            index_gen = end_point
+            index_parent = index_gen
+            while index_gen != start_point:
+
+                if parent1.genotype[index_parent] not in genotype:
+                    genotype[index_gen] = parent1.genotype[index_parent]
+                    index_parent = index_parent + 1
+                    index_gen = index_gen + 1
+                else:
+                    index_parent = index_parent + 1
+                if index_parent >= len(parent1.genotype):
+                    index_parent = 0
+                if index_gen >= len(parent1.genotype):
+                    index_gen = 0
 
             # Creamos un nuevo individuo con el genotipo resultante
-            child = Individual(np.array(child_genotype))
+            child = Individual(np.array(genotype))
             new_population.append(child)
 
         return new_population
 
 
-# Crear instancias de Individual y asignarles un valor de fitness
-individuals = []
-for _ in range(2):
-    genotype = np.random.randint(
-        0, 2, size=(10,)
-    )  # Ejemplo de un genotipo aleatorio de tamaño 10
-    fitness_value = np.random.uniform(
-        0, 1
-    )  # Ejemplo de un valor de fitness aleatorio entre 0 y 1
-    individual = Individual(genotype)
-    individual.fitness = fitness_value
-    individuals.append(individual)
+# # Crear instancias de Individual y asignarles un valor de fitness
+# individuals = []
+# for _ in range(2):
+#     permutation = np.random.permutation(
+#         11
+#     )  # 11 porque la permutación excluye el último número
 
-print(*map(lambda x: x.__dict__, individuals), sep="\n")
-population = Ordered.crossover(individuals)
-print(*map(lambda x: x.__dict__, population), sep="\n")
+#     # Seleccionar los primeros 10 elementos de la permutación
+#     genotype = permutation[:10]
+#     fitness_value = np.random.uniform(
+#         0, 1
+#     )  # Ejemplo de un valor de fitness aleatorio entre 0 y 1
+#     individual = Individual(genotype)
+#     individual.fitness = fitness_value
+#     individuals.append(individual)
+
+# print(*map(lambda x: x.__dict__, individuals), sep="\n")
+# population = OrderedOX1.crossover(individuals)
+# print(*map(lambda x: x.__dict__, population), sep="\n")
